f15d941dadf6c1033e8dc2320cf08c94deb3e696
Hadiur <fmhadiuzzaman@gmail.com>
2026-04-10 01:50:28 +0200

    first commit

--- src/kiosk/data/products.js
+++ src/kiosk/data/products.js
@@ -21,3 +21,38 @@ export const categories = [
       "https://images.unsplash.com/photo-1502741338009-cac2772e18bc?q=80&w=1200&auto=format&fit=crop",
   },
 ];
+
+export const products = [
+  {
+    id: 1,
+    category: "mocktail",
+    name: "Berry Velvet",
+    price: 8.9,
+    image:
+      "https://images.unsplash.com/photo-1544145945-f90425340c7e?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 2,
+    category: "mocktail",
+    name: "Mint Royale",
+    price: 9.2,
+    image:
+      "https://images.unsplash.com/photo-1470337458703-46ad1756a187?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 3,
+    category: "ice",
+    name: "Vanilla Bliss",
+    price: 5.9,
+    image:
+      "https://images.unsplash.com/photo-1570197788417-0e82375c9371?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 4,
+    category: "smoothie",
+    name: "Mango Smoothie",
+    price: 7.5,
+    image:
+      "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4?q=80&w=1200&auto=format&fit=crop",
+  },
+];
